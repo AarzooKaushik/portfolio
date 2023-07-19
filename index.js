d2c19bfbdc60053c35246f1899b6ddcba95921a5
AarzooKaushik <aarzookaushik31@gmail.com>
2023-07-19 13:25:59 +0530

    commit

--- index.js
+++ index.js
@@ -61,43 +61,6 @@ hideMenu.addEventListener("click", () => {
   navList.classList.remove("active-menu");
 });
 
-/////////// form validation /////////////
-document.getElementById("myForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var phone = document.getElementById("phone").value;
-  var message = document.getElementById("message").value;
-  var errorMessage = document.querySelector(".error-message");
-  var isValid = true;
-
-  errorMessage.innerHTML = "";
-
-  if (
-    name.trim() === "" ||
-    email.trim() === "" ||
-    phone.trim() === "" ||
-    message.trim() === "" ||
-    !validateEmail(email)
-  ) {
-    errorMessage.innerHTML = "Please check your inputs .";
-    isValid = false;
-  }
-
-  if (isValid) {
-    errorMessage.innerHTML = "";
-
-    this.submit();
-    this.reset();
-    alert("Form submitted successfully!");
-  }
-});
-
-function validateEmail(email) {
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
 /////////// animation /////////////
 
 const timeline = gsap.timeline();
@@ -183,4 +146,101 @@ window.addEventListener("scroll", () => {
       }
     }
   });
+});
+
+///////////////////////////////////////////////
+
+emailjs.init("58sbKSHw7RiXDWyBC");
+
+const form = document.querySelector("#myForm");
+const username = document.querySelector("#username");
+const phone = document.querySelector("#phone");
+const email = document.querySelector("#email");
+function isemail(email) {
+  return /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test(
+    email
+  );
+}
+function validatePhoneNumber(phone) {
+  var phoneRegex = /^(\d{3}[-\s]?\d{3}[-\s]?\d{4})$/;
+  return phoneRegex.test(phone);
+}
+const validate = {
+  username: (value) => {
+    if (!value) {
+      return true;
+    }
+    return false;
+  },
+
+  email: (value) => {
+    if (!value) {
+      return true;
+    }
+    if (!isemail(value)) {
+      return true;
+    }
+    return false;
+  },
+
+  phone: (value) => {
+    if (!value) {
+      return true;
+    }
+    if (!validatePhoneNumber(value)) {
+      return true;
+    }
+    return false;
+  },
+};
+
+const setError = (selector, value) => {
+  const error = document.querySelector(`#${selector}`);
+  const hasError = validate[selector](value);
+  error.parentElement.classList.toggle("form-error", hasError);
+  return hasError;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let tempObj = {
+    username: username.value,
+    phone: phone.value,
+    email: email.value,
+  };
+
+  let hasError = false;
+
+  Object.keys(tempObj).forEach((key) => {
+    if (setError(key, tempObj[key])) {
+      hasError = true;
+    }
+  });
+
+  Object.keys(tempObj).forEach((key) => {
+    const node = document.querySelector(`#${key}`);
+    node.addEventListener("change", (e) => {
+      setError(key, e.target.value);
+    });
+  });
+
+  if (!hasError) {
+    const formData = new FormData(event.target);
+    const data = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+      message: formData.get("message"),
+    };
+
+    emailjs.send("service_u9bdohd", "template_jypynkr", data).then(
+      function (response) {
+        form.reset();
+        alert("Form submitted successfully.");
+      },
+      function (error) {
+        alert("Failed to send email:", error);
+      }
+    );
+  }
 });
